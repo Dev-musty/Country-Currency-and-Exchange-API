f9cd8621d307aa1c9b09c3631e51d48d21ffa5d8
Mustapha Ridwan <mustapharid19@gmail.com>
2025-10-26 21:32:20 +0100

    Implement all neccessary logic and routes

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import env from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import { dbInit } from './database/dbInit.js';
+import currencyExchangeRoutes from './routes/currencyExchangeRoutes.js';
 
-// cofiguraation
-env.config()
-const app = express()
-const port = process.env.PORT
+// configuration
+env.config();
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middlewares
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({
-  methods: ['GET','POST','PATCH','DELETE'],
-  allowedHeaders: ['contentType']
-}))
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
-app.listen(port, ()=>{
-  console.log(`App runnig on port -> ${port}`)
-})
+// Initialize DB
+dbInit();
+
+// routes
+app.use('/', currencyExchangeRoutes);
+
+app.listen(port, async() => {
+  console.log(`App running on port -> ${port}`);
+});
